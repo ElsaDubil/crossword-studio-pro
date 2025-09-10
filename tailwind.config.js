d2b9954eeb599f47b4070e11c9b83d0d8a58b7c6
ElsaDubil <elsa.dubil@gmail.com>
2025-09-10 12:44:06 -0700

    Initial React app setup with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'system-ui', 'sans-serif'],
+        'mono': ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
